ac5ff37bbbaa121b66029f3f3089954e338d0298
rivalcoba <ivan.rivalcoba@gmail.com>
2021-03-18 11:57:58 -0600

    Proyecto en estado inicial funcionando

---
+++ routes/index.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express', author: 'Jorge Rivalcoba', appName: 'WebApp' });
+});
+
+/* Agregando nueva ruta */
+router.get('/greeting', function(req, res, next){
+  res.status(200).json({message: 'Hola Campeon de la Fullstack Web'})
+})
+
+module.exports = router;
