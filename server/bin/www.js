11858dadbe3ea63e80f9b57dc732ecfeda01feeb
rivalcoba <ivan.rivalcoba@gmail.com>
2021-06-22 13:36:20 -0500

    S38 Dotenv

--- server/bin/www.js
+++ server/bin/www.js
@@ -1,5 +1,7 @@
 #!/usr/bin/env node
 import winston from '@server/config/winston';
+// Importando configuraciones de apicacion
+import configKeys from '@server/config/configKeys';
 
 /**
  * Module dependencies.
@@ -34,7 +36,7 @@ function normalizePort(val) {
  * Get port from environment and store in Express.
  */
 
-const port = normalizePort(process.env.PORT || '3000');
+const port = normalizePort(configKeys.port || '3000');
 app.set('port', port);
 
 const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`;
---
+++ server/config/configKeys.js
@@ -0,0 +1,13 @@
+// Importando el paquete DotEnv
+import dotenv from 'dotenv';
+
+// Cargar las variables de entorno
+dotenv.config();
+
+// Exportando los valores de
+// configuracion
+export default {
+  homeUrl: `${process.env.APP_URL}:${process.env.PORT}`,
+  port: process.env.PORT,
+  ip: process.env.IP,
+};
