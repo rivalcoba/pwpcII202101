dca3efc96516508860c5744e981c542d5153ed27
rivalcoba <ivan.rivalcoba@gmail.com>
2021-09-13 11:10:23 -0500

    Sesion 40 Conexion a base de datos Done!

--- server/config/odm.js
+++ server/config/odm.js
@@ -1,25 +1,28 @@
+// Importando biblioteca ODM
 import mongoose from 'mongoose';
+// Importando script para el log
 import winston from './winston';
 
 class MongooseODM {
-  // Constructor de la clase
+  // Constructor
   constructor(url) {
     this.url = url;
   }
 
-  // Metodos
+  // Metodo de conexion
   async connect() {
+    // Sustituyendo el sistema de promesas
+    // de mongoose por el Javascript
+    mongoose.Promise = global.Promise;
+    winston.info(`☎ Conectando a la base de datos en: ${this.url}`);
     try {
-      // Configuraciones que requiere mongoose
-      mongoose.Promise = global.Promise;
-      mongoose.set('useNewUrlParser', true);
-      mongoose.set('useFindAndModify', false);
-      mongoose.set('useCreateIndex', true);
-      mongoose.set('useUnifiedTopology', true);
-      winston.info(`🚠 Conectado a la DB en: ${this.url}`);
       await mongoose.connect(this.url);
       return true;
     } catch (error) {
+      winston.error(
+        `❌ Error al conectarse a la base de datos: ${error.message}`
+      );
+      // Se retorna false en caso de que no se realice una conexión exitosa
       return false;
     }
   }
