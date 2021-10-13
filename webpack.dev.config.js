1fdb25380c74d9d83b4f37602829e05b3d8b7836
rivalcoba <ivan.rivalcoba@gmail.com>
2021-10-13 14:03:59 -0500

    Sesion 25 Instalación de reglas preconfiguradas

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,23 +1,24 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const ESLintPlugin = require("eslint-webpack-plugin");
 module.exports = {
   // 0. Establecer el modo del configurador
-  mode: 'development',
+  mode: "development",
   // 1. Especificando el archivo de entrada
-  entry: './client/index.js',
+  entry: "./client/index.js",
   // 2. Especificando la salida
   output: {
     // 3. Ruta absoluta de la salida
-    path: path.join(__dirname, 'public'),
+    path: path.join(__dirname, "public"),
     // 4. Nombre del archivo de salida
-    filename: 'js/bundle.js',
+    filename: "js/bundle.js",
     // 5. Ruta del path publico para fines del servidor de desarrollo
-    publicPath: '/'
+    publicPath: "/",
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public'),
-    port: process.env.PORT || '3000',
-    host: 'localhost'
+    contentBase: path.join(__dirname, "public"),
+    port: process.env.PORT || "3000",
+    host: "localhost",
   },
   module: {
     rules: [
@@ -26,43 +27,44 @@ module.exports = {
         exclude: /(node_modules|bower_components)/,
         use: [
           {
-            loader: 'babel-loader',
+            loader: "babel-loader",
             options: {
               presets: [
                 [
-                  '@babel/preset-env',
+                  "@babel/preset-env",
                   {
-                    'modules': false,
-                    'useBuiltIns': 'usage',
-                    'targets': {"chrome": "80"},
-                    'corejs': 3
-                  }
-                ]
+                    modules: false,
+                    useBuiltIns: "usage",
+                    targets: { chrome: "80" },
+                    corejs: 3,
+                  },
+                ],
               ],
-              "plugins": [
+              plugins: [
                 [
                   "module-resolver",
                   {
-                    "root": ["./"],
-                    "alias":{
-                      "@client" : "./client",
-                    }
-                  }
-                ]
-              ]
-            }
-          }
-        ]
+                    root: ["./"],
+                    alias: {
+                      "@client": "./client",
+                    },
+                  },
+                ],
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'css-loader']
-      }
-    ]
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'styles/app.css'
-    })
-  ]
-}
+      filename: "styles/app.css",
+    }),
+    new ESLintPlugin(),
+  ],
+};
