f0c3e1a8abefaacdef1f4be3668dd3d7c4f563e6
rivalcoba <ivan.rivalcoba@gmail.com>
2021-10-13 14:55:47 -0500

    Sesion 26 airbnb

--- client/index.js
+++ client/index.js
@@ -1,24 +1,24 @@
-import './stylesheets/style.css'
-import './stylesheets/mystyles.css'
+import './stylesheets/style.css';
+import './stylesheets/mystyles.css';
 
-console.log("Webpack Working!!!");
+console.log('Webpack Working!!!');
 // Default parameters ES6/2015
-let show = (m = "Hot Module Replacement Working") => {
-  alert(m)
+const show = (m = 'Hot Module Replacement Working') => {
+  alert(m);
 };
 
 show();
 
-function resolveAfter2Seconds(){
-  return new Promise( resolve => {
-    setTimeout(()=>{
-      resolve('resolved')
+function resolveAfter2Seconds() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve('resolved');
     }, 2000);
   });
 }
 
-async function asyncCall(){
-  console.log("Calling an async function");
+async function asyncCall() {
+  console.log('Calling an async function');
   const result = await resolveAfter2Seconds();
   console.log(result);
 }
