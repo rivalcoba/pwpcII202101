9807d9a115a33ca8dc130c0ea16b663d63149f70
rivalcoba <ivan.rivalcoba@gmail.com>
2021-11-22 18:53:14 -0600

    S46 Vista con validación

--- server/controllers/projectController.js
+++ server/controllers/projectController.js
@@ -14,8 +14,28 @@ const add = (req, res) => {
 // POST "/projects/add"
 const addPost = (req, res) => {
   // Rescatando la información del formulario
-  const { validData: project } = req;
-  res.status(200).json(project);
+  const { validData, errorData } = req;
+  // Creando view models
+  let project = {};
+  let errorModel = {};
+  // Verificando si hay errores
+  if (errorData) {
+    // Rescatando objeto validado
+    project = errorData.value;
+    // Usamos reduce para generar
+    // un objeto de errores
+    // apartir del arreglo inner
+    errorModel = errorData.inner.reduce((prev, curr) => {
+      // Creamos una variable temporal para evitar
+      // el error "no-param-reassing"
+      const newVal = prev;
+      newVal[`${curr.path}Error`] = curr.message;
+      return newVal;
+    }, {});
+  } else {
+    project = validData;
+  }
+  res.render('project/addView', { project, errorModel });
 };
 
 // Pendiente por programar
--- server/validators/validateFactory.js
+++ server/validators/validateFactory.js
@@ -11,12 +11,13 @@ const Validator =
       const validData = await shape.validate(dataObject, { abortEarly: false });
       // Se inyecta el objeto validado a la petición
       req.validData = validData;
-      // Se invoca al siguiente middleware de la cadena
-      return next();
     } catch (error) {
-      // En caso de error se regresa la info del error
-      return res.status(400).json({ error });
+      // En caso de error agregar el objeto de error de yup
+      // en la petición
+      req.errorData = error;
     }
+    // Se invoca al siguiente middleware de la cadena
+    return next();
   };
 
 // exportando el validador
